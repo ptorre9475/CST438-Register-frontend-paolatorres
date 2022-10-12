3315891ae82ecd3fd7806a5b3251dba514f6cb39
= <=>
2022-10-11 22:30:50 -0700

    Changes for Assignment OAuth2

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -6,13 +6,15 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
 import {DataGrid} from '@mui/x-data-grid';
-import {SEMESTER_LIST} from '../constants.js'
+import {SEMESTER_LIST} from '../constants.js';
+import Login from './Login';
 
 // user selects from a list of  (year, semester) values
 class Semester extends Component {
+	
     constructor(props) {
       super(props);
-      this.state = {selected: SEMESTER_LIST.length-1 };
+      this.state = {selected: SEMESTER_LIST.length-1, adminStatus: false};
     }
  
    onRadioClick = (event) => {
@@ -29,7 +31,7 @@ class Semester extends Component {
         renderCell: (params) => (
           <div>
             <Radio
-              checked={params.row.id == this.state.selected}
+              checked={params.row.id === this.state.selected}
               onChange={this.onRadioClick}
               value={params.row.id}
               color="default"
@@ -40,36 +42,63 @@ class Semester extends Component {
         )
       },
       { field: 'name', headerName: 'Semester', width: 200 }
-      ];       
-       
-    return (
-       <div>
-         <AppBar position="static" color="default">
-            <Toolbar>
-               <Typography variant="h6" color="inherit">
-                  Schedule - select a term
-               </Typography>
-            </Toolbar>
-         </AppBar>
-         <div align="left" >
-              <div style={{ height: 400, width: '100%', align:"left"   }}>
-                <DataGrid   rows={SEMESTER_LIST} columns={icolumns} />
-              </div>                
-              <Button component={Link} 
-                      to={{pathname:'/schedule' , 
-                      year:SEMESTER_LIST[this.state.selected].year, 
-                      semester:SEMESTER_LIST[this.state.selected].name}} 
-                variant="outlined" color="primary" style={{margin: 10}}>
-                Get Schedule
-              </Button>
-			  <Button component={Link}
-				to={{pathname:'/student'}}
-				variant="outlined" color="primary">
-				Add Student
-				</Button>
-          </div>
-      </div>
-    )
+      ];    
+	
+	if (this.state.adminStatus == true) {
+		return (
+			<div>
+				<AppBar position="static" color="default">
+					<Toolbar>
+						<Typography variant="h6" color="inherit">
+							Welcome Admin
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<div align="left">
+					<div style={{ height: 400, width: '100%', align:"left"   }}>
+						<DataGrid   rows={SEMESTER_LIST} columns={icolumns} />
+					</div>                
+					<Button component={Link} 
+						to={{pathname:'/schedule' , 
+						year:SEMESTER_LIST[this.state.selected].year, 
+						semester:SEMESTER_LIST[this.state.selected].name}} 
+						variant="outlined" color="primary" style={{margin: 10}}>
+						Get Schedule
+					</Button>
+					<Button id="addStudent" component={Link}
+						to={{pathname:'/student'}}
+						variant="outlined" color="primary" style={{margin: 10}}>
+						Add Student
+					</Button>
+				</div>
+			</div>
+		)
+	}
+	else {
+		return (
+		   <div>
+			 <AppBar position="static" color="default">
+				<Toolbar>
+				   <Typography variant="h6" color="inherit">
+					  Welcome Student: Schedule - select a term
+				   </Typography>
+				</Toolbar>
+			 </AppBar>
+			 <div align="left" >
+				  <div style={{ height: 400, width: '100%', align:"left"   }}>
+					<DataGrid   rows={SEMESTER_LIST} columns={icolumns} />
+				  </div>                
+				  <Button component={Link} 
+						  to={{pathname:'/schedule' , 
+						  year:SEMESTER_LIST[this.state.selected].year, 
+						  semester:SEMESTER_LIST[this.state.selected].name}} 
+					variant="outlined" color="primary" style={{margin: 10}}>
+					Get Schedule
+				  </Button>
+			  </div>
+		  </div>
+		)
+	}
   }
 }
 export default Semester;
